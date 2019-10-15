2b4794fdab0b82d2fe035742523d1f66b9847dce
Finnbogi Jakobsson <finnbogi18@ru.is>
2019-10-15 15:16:42 +0000

    Algoritmi

---
+++ paragraph_analysis.py
@@ -0,0 +1,3 @@
+""" Reads a file input by the user and analyses the paragraphs, finds all the unique words,
+    prints out each unique word and in which paragraphs that word is and finds out how many the word
+    occured totally in all paragraphs together. """
